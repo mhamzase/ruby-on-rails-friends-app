c55a6f7e4d43efb31b57807ed6ff3cd1938d4462
mhamzase <mhamzasulehri143@gmail.com>
2022-02-03 14:53:34 +0500

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  devise_for :users
+  resources :friends
+  # get 'home/index'
+  get 'home/about'
+  root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
